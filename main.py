dced6239922e97cb1f384615f0670aa8118283dd
beni048 <beni048@hotmail.com>
2024-09-07 17:18:26 +0200

    60 fps

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
 
@@ -19,6 +18,8 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
         main()
